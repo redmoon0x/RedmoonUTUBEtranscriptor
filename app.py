a01ee8976dcf7fc285c8860e379de7276864e445
Redmoon <102231335+redmoon0x@users.noreply.github.com>
2024-07-23 15:50:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-# app.py
 from flask import Flask, request
 import os
-from youbot import bot  # Import the bot instance
-import time 
+import time
+from youbot import bot  # Make sure 'youbot.py' correctly initializes the 'bot' object from pyTelegramBotAPI
+import telebot  # This is necessary to use telebot.types for deserializing JSON
 
-WEBHOOK_URL = 'https://redmoonutubetranscriptor.onrender.com/bot'  # Replace <your-domain> with your actual domain
+WEBHOOK_URL = 'https://redmoonutubetranscriptor.onrender.com/bot'
 
 app = Flask(__name__)
 
 @app.route('/bot', methods=['POST'])
 def webhook():
     if request.headers.get('content-type') == 'application/json':
-        json_string = request.get_data().decode('utf-8')
+        json_string = request.get_data(as_text=True)  # Using as_text=True to directly get string
         update = telebot.types.Update.de_json(json_string)
         bot.process_new_updates([update])
         return '', 200
@@ -22,10 +22,7 @@ def index():
     return '<h1>The bot is running</h1>'
 
 if __name__ == '__main__':
-    bot.remove_webhook()
-    time.sleep(1)  # Ensure the removal is processed
-    response = bot.set_webhook(url=WEBHOOK_URL)
-    if response:  # Log the response to see if the webhook is set successfully
-        print("Webhook set successfully.")
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
+    bot.remove_webhook()  # Clear any previously set webhook
+    time.sleep(1)  # Wait to ensure webhook is removed
+    bot.set_webhook(url=WEBHOOK_URL)  # Set the new webhook for the bot
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))  # Ensure using PORT env var or default to 5000
